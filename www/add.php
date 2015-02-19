6202e9fd006edbeafecd964975b3871375b17d7e
AlexanderDenisov <ad060778@gmail.com>
2015-02-19 21:12:59 +0300

    Добавили add.php / Контроллер обрабатывающий запрос из формы

---
+++ www/add.php
@@ -0,0 +1,27 @@
+<?php
+require __DIR__ . '/models/news.php';
+require __DIR__ . '/function/file.php';
+
+//Контроллер, обрабатывает запрос из формы добавления новости.
+if (!empty($_POST)) {
+    $data = [];
+    if (!empty($_POST['title'])) {
+        $data['title'] = $_POST['title'];
+    }
+    if (!empty($_POST['date'])) {
+        $data['date'] = $_POST['date'];
+    }
+    if (!empty($_FILES)) {
+        $res = File_upload('news');
+        if (false !== $res) {
+            $data['news'] = $res;
+        }
+    }
+    if (isset($data['date']) && isset($data['title']) && isset($data['news'])) {
+        News_insert($data);
+        header('Location: /index.php');
+        die;
+    }
+}
+include __DIR__ . '/views/add.php';
+?>
