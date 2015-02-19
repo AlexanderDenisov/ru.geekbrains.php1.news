4c57e126f5a18da06e0884e49858a3aac59bf0b5
AlexanderDenisov <ad060778@gmail.com>
2015-02-19 21:21:38 +0300

    Создали папку function. Добавили в нее file.php / Функция для загрузки файлов на сервер.

---
+++ www/function/file.php
@@ -0,0 +1,21 @@
+<?php
+
+//Функция загрузки файлов на сервер.
+function File_upload($field)
+{
+    if (empty($_FILES))
+        return false;
+    if (0 != $_FILES[$field]['error'])
+        return false;
+    if (is_uploaded_file($_FILES[$field]['tmp_name'])) {
+        $res = move_uploaded_file($_FILES[$field]['tmp_name'], __DIR__ . '/../news/' . $_FILES[$field]['name']);
+        if (!$res) {
+            return false;
+        } else {
+            return '/news/' . $_FILES[$field]['name'];
+        }
+    }
+    return false;
+}
+
+?>
