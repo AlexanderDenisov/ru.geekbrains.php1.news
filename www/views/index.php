42f069bf7b206b8c20324eb459d1872d908b25c8
AlexanderDenisov <ad060778@gmail.com>
2015-02-21 23:31:28 +0300

    /views/index.php добавлена проверка доступен ли файл для чтения.

--- www/views/index.php
+++ www/views/index.php
@@ -19,15 +19,19 @@
                 <td><?php echo $item['date']; ?></td>
                 <td><?php echo $item['title']; ?></td>
                 <td><?php $home = $item['path'];
+                    if (is_readable(__DIR__ . "/../$home")) {
                     $homepage = file_get_contents(__DIR__ . "/../$home", NULL, NULL, 0, 140);
                     echo $homepage; ?><br><a href="/../fullnews.php/?id=<?php echo $item['id']; ?>">читать новость
                         полностью >> </a></td>
+                <?php } else {
+                    echo 'Файл не доступен для чтения.';
+                } ?>
             </tr>
         <?php endforeach; ?>
         </tr>
     </table>
 </center>
-<a href="/add.php">Добавить новость</a>
+<!-- <a href="/add.php">Добавить новость</a> -->
 <?php include __DIR__ . '/form.php'; ?>
 
 </body>
