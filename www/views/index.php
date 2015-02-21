d3a907853e78ad637e0a11950da40b31e3277273
AlexanderDenisov <ad060778@gmail.com>
2015-02-21 13:04:45 +0300

    Изменил /views/index.php Добавлена возможность чтения нескольких символов из файла.

--- www/views/index.php
+++ www/views/index.php
@@ -11,13 +11,17 @@
             <th>Название</th>
             <th>Текст новости</th>
         </tr>
+        <tr>
+            <th><BR></th>
+            </tr>
         <?php foreach ($items as $item): ?>
             <tr>
                 <td><?php echo $item['date']; ?></td>
                 <td><?php echo $item['title']; ?></td>
                 <td><?php $home = $item['path'];
-                    $homepage = file_get_contents(__DIR__ . "/../$home");
-                    echo $homepage; ?></td>
+                    $homepage = file_get_contents(__DIR__ . "/../$home", NULL, NULL, 0, 140);
+                    echo $homepage; ?><br><a href="/index.php/?id=<?php echo $item['id']; ?>">читать новость
+                        полностью</a></td>
             </tr>
         <?php endforeach; ?>
         </tr>
