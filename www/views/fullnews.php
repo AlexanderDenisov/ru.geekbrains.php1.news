b22ee1da6d7c6f4d607a3b0628987b8acff0852d
AlexanderDenisov <ad060778@gmail.com>
2015-02-21 23:29:54 +0300

    /views/fullnews.php добавлена проверка доступен ли файл для чтения.

--- www/views/fullnews.php
+++ www/views/fullnews.php
@@ -18,8 +18,12 @@
             <td><?php echo $full_new['date']; ?></td>
             <td><?php echo $full_new['title']; ?></td>
             <td><?php $home = $full_new['path'];
+                if (is_readable(__DIR__ . "/../$home")) {
                 $homepage = file_get_contents(__DIR__ . "/../$home");
                 echo $homepage; ?><br><a href="/../index.php/">Назад << </a></td>
+            <?php } else {
+                echo 'Файл не доступен для чтения.';
+            } ?>
         </tr>
     <?php endforeach; ?>
     </tr>
