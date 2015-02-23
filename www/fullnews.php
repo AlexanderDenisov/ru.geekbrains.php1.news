3e8f71a93f6aa820bbf7d01fa00c0378ff1a3d0f
AlexanderDenisov <ad060778@gmail.com>
2015-02-23 14:18:29 +0300

    /fullnews.php добавлен метод GET

--- www/fullnews.php
+++ www/fullnews.php
@@ -3,7 +3,13 @@
 
 require_once __DIR__ . '/models/news.php';
 
-$full_news = News_getOnlyChoosen();
+if(!isset($_GET['id'])) {
+    echo 'Такой новости нет.';
+    die;
+} else {
+    $news_id = $_GET['id'];
+    $full_news = News_getOnlyChoosen($news_id);
+}
 
 include __DIR__ . '/views/fullnews.php';
 
